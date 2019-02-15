e41ad4aa19104c11cc183a4f91afa8245c0ad4aa
Ihor Petruniv <ihor_petruniv@epam.com>
2019-02-15 15:00:57 +0200

    Initial commit

--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,5 @@
 #a simple program with lyrics from Mary Had a Little Lamb
-print "Mary had a little lamb,"
-print "it's fleece was white as snow;"
-print "and everywhere that Mary went",
-print "her lamb was sure to go."
+print('Mary had a little lamb')
+print('it\'s fleece was white as snow;')
+print('and everywhere that Mary went')
+print('her lamb was sure to go.')
